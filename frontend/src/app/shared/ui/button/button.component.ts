9a0bdacea9234316c7cb436f4b6dbc971b53533a
dunas26 <jhonjamesillo@gmail.com>
2024-09-12 20:26:59 -0500

    feat(frontend): add button component

---
+++ frontend/src/app/shared/ui/button/button.component.ts
@@ -0,0 +1,29 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+type ButtonStyle = "primary" | "secondary";
+
+@Component({
+	selector: 'app-button',
+	standalone: true,
+	imports: [NgClass],
+	templateUrl: './button.component.html',
+	styles: ``
+})
+export class ButtonComponent {
+
+	@Input({ required: true }) public label: string = "";
+	@Input() public loading: boolean = false;
+	@Input({ alias: "type" }) public set buttonType (value: ButtonStyle) {
+		value = value || "primary";
+		const classMap: { [key: string]: string } = {
+			primary: "bg-primary text-quaternary",
+			secondary: "bg-tertiary text-quaternary"
+		};
+		this.buttonClasses = classMap[value];
+	};
+
+	public buttonClasses: string = "bg-primary text-quaternary";
+
+
+}
